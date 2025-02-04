46b5cc758a10a9393153643adab6446a1735937d
Joydip Das <190102.cse@student.just.edu.bd>
2025-02-04 12:22:54 +0600

    added Customer

--- person/urls.py
+++ person/urls.py
@@ -1,7 +1,12 @@
 from django.urls import path
-from .views import MohajonListCreateAPIView, MohajonDestroyUpdateAPIView
+from .views import*
 
 urlpatterns = [
     path('mohajons/', MohajonListCreateAPIView.as_view(), name='mohajon-list-create'),
     path('mohajons/<pk>/', MohajonDestroyUpdateAPIView.as_view(), name='mohajon-destroy-update'),
+    path('customers/', CustomerListCreateAPIView.as_view(), name='customer-list-create'),
+    path('customers/<pk>/', CustomerDestroyUpdateAPIView.as_view(), name='customer-destroy-update'),
+
+
+    
 ]
