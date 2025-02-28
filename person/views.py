c4a396fc7ac367e58d7a9fa1246e60738b1639aa
Joydip Das <joydipd381@gmail.com>
2025-02-28 21:45:30 +0600

    Permission class added and validated

--- person/views.py
+++ person/views.py
@@ -28,11 +28,6 @@ class IsAdmin(BasePermission):
     def has_permission(self, request, view):
         return request.user.is_authenticated and request.user.role == 'admin'
 
-# class IsAdminOrAuthenticated(BasePermission):
-#     def has_permission(self, request, view):
-#         # Allow access if user is authenticated or has admin role
-#         return request.user.is_authenticated and (request.user.is_staff or request.user.is_superuser)
-
 class UserRegistrationView(ListCreateAPIView):
     permission_classes = [AllowAny]
 
